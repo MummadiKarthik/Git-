e4947e23ea7c4dbb83a877c66e1d42d7d894e88c
Mummadi Karthik <mummadi.karthik2000@gmail.com>
2026-09-07 16:33:24 +0530

    asd

--- hello.js
+++ hello.js
@@ -9,3 +9,13 @@ function kar(a){
 let s=kar(24);
 let a=s(35);
 console.log(a(26))
+
+function kaar(call){
+    let a=24;
+    let b=34;
+    call(a,b)
+}
+function bar(a,b){
+    console.log(a+b)
+}
+kaar(bar)
