805e783bdcd5d2bebc71c2318cbfc89eeda839d4
MUMMADI KARTHIK <131574850+MummadiKarthik@users.noreply.github.com>
2026-09-14 19:40:41 +0530

    Add array filtering and mapping examples
    Added filtering of even numbers from the adf array and logging of elements from the s array.

--- bro.js
+++ bro.js
@@ -11,6 +11,7 @@ const calc = new Calculator();
 console.log(calc.add(1, 2));    // 3
 console.log(calc.add(1, 2, 3)); // 6
 
-
+let adf=[234,12,45,67];
+adf.filter((s)=>s%2==0?s:0)
 let s=[25,12,58,36];
 s.map((a)=>console.log(a))
